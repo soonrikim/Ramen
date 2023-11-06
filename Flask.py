2651d447ca094202838b42fb23a18b755abf45b8
soonrikim <soon921111@gmail.com>
2023-11-06 21:53:00 +0900

    finished

--- Flask.py
+++ Flask.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, jsonify
 import json
+
+from tabelog_scraper import Tabelog
+
 app = Flask(__name__, template_folder='templates')
 
 
@@ -15,6 +18,36 @@ store_data = load_data_from_json('tabelog_data.json')
 @app.route('/')
 def index():
     return render_template('index.html', store_list=store_data)
+@app.route('/get_stores_by_station_and_rating/<station>/<rating>')
+def get_stores_by_station_and_rating(station, rating):
+    with open('tabelog_data.json') as file:
+        data = json.load(file)
+        filtered_data = [store for store in data if store.get('station') == station and store.get('score') >= float(rating)]
+    return jsonify(filtered_data)
+
+@app.route('/get_data/shinjuku')
+def get_data_shinjuku():
+    tabelog_shinjuku = Tabelog(p_ward='新宿')
+    data_shinjuku = tabelog_shinjuku.crawl_data()
+    return jsonify(data_shinjuku)
+
+@app.route('/get_data/shinokubo')
+def get_data_shinokubo():
+    tabelog_shinokubo = Tabelog(p_ward='新大久保')
+    data_shinokubo = tabelog_shinokubo.crawl_data()
+    return jsonify(data_shinokubo)
+
+@app.route('/get_data/ogikubo')
+def get_data_ogikubo():
+    tabelog_ogikubo = Tabelog(p_ward='荻窪')
+    data_ogikubo = tabelog_ogikubo.crawl_data()
+    return jsonify(data_ogikubo)
+
+@app.route('/get_data/shinagawa')
+def get_data_shinagawa():
+    tabelog_shinagawa = Tabelog(p_ward='品川')
+    data_shinagawa = tabelog_shinagawa.crawl_data()
+    return jsonify(data_shinagawa)
 
 
 @app.route('/read/<int:id>/')
